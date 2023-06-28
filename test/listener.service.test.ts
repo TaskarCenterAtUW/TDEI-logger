ee8dc55346c4f4be3965742bc0225bdee8641964
Naresh Kumar D <susrisha@gmail.com>
2023-06-28 13:31:46 +0530

    Unit  testing initiated

---
+++ test/listener.service.test.ts
@@ -0,0 +1,24 @@
+import { mock } from "node:test";
+import { MockDataService } from "./mock/mock-data-service"
+import {ListenerService} from '../src/services/listener.service';
+import { QueueMessage } from "nodets-ms-core/lib/core/queue";
+
+/**
+ * Unit testing for listener service.
+ * There is only one method to be tested appropriately
+ */
+describe('Listener service', ()=>{
+    describe('Functional', ()=>{
+        const mocDataService = new MockDataService();
+
+        test('When message is received, expect database to process that message', ()=>{
+            const listener = new ListenerService(mocDataService);
+            const message = QueueMessage.from({messageId:'sample'});
+            const processMessageSpy = jest.spyOn(mocDataService,'processMessage');
+            listener.onReceive(message);
+            expect(processMessageSpy).toHaveBeenCalledTimes(1);
+            expect(processMessageSpy).toHaveBeenCalledWith(message);
+
+        })
+    })
+})
---
+++ test/status-controller.test.ts
@@ -0,0 +1,65 @@
+/**
+ * Tests for `status-controller.ts`
+ * It has two paths 
+ * - getStatus and
+ * - getStageReport
+ * Both will take tdeiRecord as parameter.
+ */
+
+import { getMockReq, getMockRes } from "@jest-mock/express"
+// Need to mock the database service
+import { StatusController } from "../src/controller/status-controller";
+import { MockDataService } from './mock/mock-data-service';
+
+
+
+describe('Status Controller Test', () => {
+    const dataService = new MockDataService();
+    describe('Get Status', () => {
+        describe('Functional', () => {
+            test('When requested with tdeiRecord, Expect a record to be sent', async () => {
+                const req = getMockReq({ query: { tdeiRecordId: 'abc' } });
+                const { res, next } = getMockRes();
+                const getStatusSpy = jest.spyOn(dataService, 'getStatus');
+                const statusController = new StatusController(dataService);
+                await statusController.getStatus(req, res);
+                expect(res.status).toHaveBeenCalledWith(200);
+                expect(getStatusSpy).toHaveBeenCalledTimes(1);
+
+            })
+            test('When requested with empty recordId, Expect 404 error', async () => {
+                const req = getMockReq({ query: { tdeiRecordId: '' } });
+                const { res, next } = getMockRes();
+                const statusController = new StatusController(dataService);
+                await statusController.getStatus(req, res);
+                expect(res.status).toHaveBeenCalledWith(404);
+                expect(res.send).toHaveBeenCalledWith(expect.stringContaining("Record Id is empty"))
+
+            })
+        })
+    })
+    describe('Get Stage Report', ()=>{
+        describe('Functional', ()=>{
+            test('When requested with tdeiRecord, Expect record to be sent', async ()=>{
+                    const req = getMockReq({ query: { tdeiRecordId: 'abc' } });
+                    const { res, next } = getMockRes();
+                    const getStatusSpy = jest.spyOn(dataService, 'getStatus');
+                    const statusController = new StatusController(dataService);
+                    await statusController.getStageReport(req, res);
+                    expect(res.status).toHaveBeenCalledWith(200);
+                    expect(getStatusSpy).toHaveBeenCalledTimes(1);
+    
+
+            })
+            test('When requested with empty recordId, Expect 404 error', async () => {
+                const req = getMockReq({ query: { tdeiRecordId: '' } });
+                const { res, next } = getMockRes();
+                const statusController = new StatusController(dataService);
+                await statusController.getStageReport(req, res);
+                expect(res.status).toHaveBeenCalledWith(404);
+                expect(res.send).toHaveBeenCalledWith(expect.stringContaining("Record Id is empty"))
+
+            })
+        })
+    })
+})
