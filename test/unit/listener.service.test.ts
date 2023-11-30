44ffa713f5d28ed9b1f476ceca2b5d4830895dbb
Naresh Kumar D <susrisha@gmail.com>
2023-11-30 14:11:08 +0530

    Update listener.service.test.ts
    Fixed unit test issues

--- test/unit/listener.service.test.ts
+++ test/unit/listener.service.test.ts
@@ -14,13 +14,27 @@ describe('Listener service', ()=>{
         test('When message is received, expect database to process that message', ()=>{
             // Arrange
             const listener = new ListenerService(mocDataService);
-            const message = QueueMessage.from({messageId:'sample'});
+            const message = QueueMessage.from({messageId:'sample',messageType:'gtfs-flex-upload'});
             const processMessageSpy = jest.spyOn(mocDataService,'processMessage');
             // Act
             listener.onReceive(message);
             // Assert
             expect(processMessageSpy).toHaveBeenCalledTimes(1);
             expect(processMessageSpy).toHaveBeenCalledWith(message);
+
+        })
+
+        test('When confidence message is received, expect database to call processConfidence Message', ()=>{
+
+             // Arrange
+             const listener = new ListenerService(mocDataService);
+             const message = QueueMessage.from({messageId:'sample',messageType:'osw-confidence-request',data:{'jobId':"4"}});
+             const processMessageSpy = jest.spyOn(mocDataService,'processConfidenceMessage');
+             // Act
+             listener.onReceive(message);
+             // Assert
+             expect(processMessageSpy).toHaveBeenCalledTimes(1);
+             expect(processMessageSpy).toHaveBeenCalledWith(message);
 
         })
     })
